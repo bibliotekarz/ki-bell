7c24e76fd52eff5b1df107dcf44b1c216886ce72
bibliotekarz <arek@bibliotekarz.com>
2021-07-22 13:49:55 +0200

    refactoring boot.py

--- boot.py
+++ boot.py
@@ -1,14 +1,7 @@
 import gc
 import time
-
 import network
 import webrepl
-
-webrepl.start()
-gc.collect()
-
-nazwa_sieci = "wifi"
-haslo_sieci = "pass"
 
 network.WLAN(network.AP_IF).active(False)
 sta_if = network.WLAN(network.STA_IF)
@@ -18,9 +11,12 @@ sta_if.connect('nazwa_sieci', 'haslo_sieci')
 
 count = 0
 while not sta_if.isconnected():
-	time.sleep_ms(1)
-	count += 1
-	if count==10000:
-		print('Not connected')
-		break
+    time.sleep_ms(1)
+    count += 1
+    if count == 10000:
+        print('Not connected')
+        break
 print('Config: ', sta_if.ifconfig())
+
+webrepl.start()
+gc.collect()
